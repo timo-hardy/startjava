fa670ffbd0df64e14e3776741b86a0ab471fed7f
Timur Khidirov <timos.gooogle@gmail.com>
2019-04-06 14:02:24 +0300

    Created a class with Variable

---
+++ Lesson_one/Variable.java
@@ -0,0 +1,20 @@
+public class Variable {
+
+	public static void main(String[] args) {
+		
+		String maker = "Apple";
+		String nameProduct = "MacBook Pro";
+		String serialNumber = "FVFX98P7HV22";
+		String versionOS = "10.14.4";
+		byte numberOfCores = 2;
+		String screenDiagonal = "13.3\"";
+		int price = 99_000;
+		float weight = 1.37f;
+		long batteryVersion = 3666;
+		boolean usbTypeC = true;
+		double volumeHD = 121.02;
+		char inputChannels = 50;
+		short speedRam = 2133;
+
+	}
+}
