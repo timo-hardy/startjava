66652f97ec2b697067b482451e9737b5ca383159
Timur Khidirov <timos.gooogle@gmail.com>
2019-04-06 18:56:23 +0300

    Created a class ConditionalStatement

---
+++ Lesson_one/ConditionalStatement.java
@@ -0,0 +1,36 @@
+public class ConditionalStatement {
+
+	public static void main(String[] args) {
+
+		int age = 23;
+		if (age > 20) {
+			System.out.println("I am getting older");
+		}
+
+		boolean man = true;
+		if (man) {
+			System.out.println("I was lucky");
+		}
+
+		boolean woman = true;
+		if (woman) {
+			System.out.println("It's very bad");
+		}
+
+		double myHeight = 1.73;
+		if (myHeight < 1.80) {
+			System.out.println("I will grow up");
+		} else {
+			System.out.println("I am Mikle Jordan");
+		}
+
+		char MyNameStartsWithALetter = 'T';
+		if (MyNameStartsWithALetter == 'M') {
+			System.out.println("My name does not start with - M");
+		} else if (MyNameStartsWithALetter == 'I') {
+			System.out.println("My name does not start with - I");
+		} else {
+			System.out.println("My name starts with - T");
+		}
+	}
+}
