4e1b8d7dcdb58f2686ef478fc606d2884040d45b
Timur Khidirov <timos.gooogle@gmail.com>
2019-04-07 12:26:27 +0300

    Bug fix

--- Lesson_one/Variable.java
+++ Lesson_one/Variable.java
@@ -1,14 +1,14 @@
 public class Variable {
 
-	public static void main(String[] args) {
-		byte numberOfCores = 2;
-		int price = 99_000;
-		float weight = 1.37f;
-		long batteryVersion = 3666;
-		boolean isUsbTypeC = true;
-		boolean isCamera = true;
-		double volumeHD = 121.02;
-		char inputChannels = 50;
-		short speedRam = 2134;
-	}
+    public static void main(String[] args) {
+        byte numberOfCores = 2;
+        int price = 99_000;
+        float weight = 1.37f;
+        long batteryVersion = 3666;
+        boolean isUsbTypeC = true;
+        boolean isCamera = true;
+        double volumeHD = 121.02;
+        char inputChannels = 50;
+        short speedRam = 2134;
+    }
 }
