1ff60ece67eb610907472ef33f05e8437129d901
Timur Khidirov <timos.gooogle@gmail.com>
2019-04-07 12:09:28 +0300

    bug fix in classes Variable and ConditionStatemetn

--- Lesson_one/ConditionalStatement.java
+++ Lesson_one/ConditionalStatement.java
@@ -1,36 +1,34 @@
 public class ConditionalStatement {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        int age = 23;
+        if (age > 20) {
+            System.out.println("I am getting older");
+        }
 
-		int age = 23;
-		if (age > 20) {
-			System.out.println("I am getting older");
-		}
+        boolean isMan = true;
+        if (isMan) {
+            System.out.println("I was lucky");
+        }
 
-		boolean man = true;
-		if (man) {
-			System.out.println("I was lucky");
-		}
+        if (!isMan) {
+            System.out.println("It's very bad");
+        }
 
-		boolean woman = true;
-		if (woman) {
-			System.out.println("It's very bad");
-		}
+        double myHeight = 1.73;
+        if (myHeight < 1.80) {
+            System.out.println("I will grow up");
+        } else {
+            System.out.println("I am Mikle Jordan");
+        }
 
-		double myHeight = 1.73;
-		if (myHeight < 1.80) {
-			System.out.println("I will grow up");
-		} else {
-			System.out.println("I am Mikle Jordan");
-		}
-
-		char MyNameStartsWithALetter = 'T';
-		if (MyNameStartsWithALetter == 'M') {
-			System.out.println("My name does not start with - M");
-		} else if (MyNameStartsWithALetter == 'I') {
-			System.out.println("My name does not start with - I");
-		} else {
-			System.out.println("My name starts with - " + MyNameStartsWithALetter);
-		}
-	}
+        char MyNameStartsWithALetter = 'T';
+        if (MyNameStartsWithALetter == 'M') {
+            System.out.println("My name does not start with - M");
+        } else if (MyNameStartsWithALetter == 'I') {
+            System.out.println("My name does not start with - I");
+        } else {
+            System.out.println("My name starts with - " + MyNameStartsWithALetter);
+        }
+    }
 }
