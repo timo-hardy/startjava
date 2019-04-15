71dfd1ca7b058c7281a1263b9eb9c9fd162b2e92
Timur Khidirov <timos.gooogle@gmail.com>
2019-04-15 21:17:59 +0300

    Created class Cycle and implemented a cycle For

---
+++ Lesson_one/Cycle.java
@@ -0,0 +1,9 @@
+public class Cycle {
+    public static void main(String[] args) {
+        int result = 0;
+        for (int i = 0; i < 20; i++) {
+            result = result + 1;
+            System.out.println("result = " + result);
+        }
+    }
+}
