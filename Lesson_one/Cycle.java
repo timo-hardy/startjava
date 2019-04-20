544b90c5c45426888ddec36a3e315a40bae47b0d
Timur Khidirov <timos.gooogle@gmail.com>
2019-04-20 16:27:11 +0300

    bug fix

--- Lesson_one/Cycle.java
+++ Lesson_one/Cycle.java
@@ -1,30 +1,30 @@
 public class Cycle {
     public static void main(String[] args) {
-        // int result = 0;
-        // for (int i = 0; i < 20; i++) {
-        //     result = result + 1;
-        //     System.out.println("result = " + result);
-        // }
+        int result = 0;
+        for (int i = 0; i < 20; i++) {
+            result = result + 1;
+            System.out.println("result = " + result);
+        }
 
-        // int j = 2;
-        // int result1 = 8;
-        // while (j <= 5) {
-        //     result1 = result1 - j;
-        //     if (result1 >= -6) {
-        //         System.out.println("While - " + result1);
-        //     }
-        //     j++;
-        // }
+        int j = 2;
+        int result1 = 8;
+        while (j <= 5) {
+            result1 = result1 - j;
+            if (result1 >= -6) {
+                System.out.println("While - " + result1);
+            }
+            j++;
+        }
 
-        int i = 10;
+        int k = 10;
         int sum = 0;
         do {
-            sum = sum + i;
+            sum = sum + k;
             if(sum % 2 != 0) {
                 System.out.println("The number " + sum + " is Odd");
             }
-            i++;
-        } while(i <= 20);
+            k++;
+        } while(k <= 20);
 
         System.out.println("sum of odd numbers - " + sum);
     }
