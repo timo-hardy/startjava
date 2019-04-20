e217ef7abc9827a3e7093f7cb0ebbf188b3fd786
Timur Khidirov <timos.gooogle@gmail.com>
2019-04-20 15:53:13 +0300

    implemented cycle While

--- Lesson_one/Cycle.java
+++ Lesson_one/Cycle.java
@@ -5,5 +5,17 @@ public class Cycle {
             result = result + 1;
             System.out.println("result = " + result);
         }
+
+        int j = 2;
+        int result1 = 8;
+        while (j <= 5) {
+            result1 = result1 - j;
+            if (result1 >= -6) {
+                System.out.println("While - " + result1);
+            }
+            j++;
+        }
+
+
     }
 }
