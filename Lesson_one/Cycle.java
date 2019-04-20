e7d6e7eefb498da6383db5484fe4a21d3c3c7a9d
Timur Khidirov <timos.gooogle@gmail.com>
2019-04-20 18:09:07 +0300

    Bug fix

--- Lesson_one/Cycle.java
+++ Lesson_one/Cycle.java
@@ -1,23 +1,21 @@
 public class Cycle {
     public static void main(String[] args) {
-        int result = 0;
         for (int i = 0; i < 20; i++) {
-            result = result + 1;
-            System.out.println("result = " + result);
+            System.out.println("result = " + i);
         }
 
         int j = 2;
-        int result1 = 8;
-        while (j <= 5) {
-            result1 = result1 - j;
-                System.out.println("While - " + result1);
+        int result1 = -8;
+        while (j < 6 && j >= -6) {
+            result1 = result1 + j;
+            System.out.println("While - " + result1);
             j++;
         }
 
         int k = 11;
         int sum = 0;
         do {
-            sum = sum + k;
+            sum += k;
             k += 2;
         } while(k < 20);
 
