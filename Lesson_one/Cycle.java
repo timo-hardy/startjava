6590dff6f10f6f584fd2e6aa58f58d5d3c927d87
Timur Khidirov <timos.gooogle@gmail.com>
2019-04-20 16:24:02 +0300

    implemented the cycle dowhile

--- Lesson_one/Cycle.java
+++ Lesson_one/Cycle.java
@@ -1,21 +1,31 @@
 public class Cycle {
     public static void main(String[] args) {
-        int result = 0;
-        for (int i = 0; i < 20; i++) {
-            result = result + 1;
-            System.out.println("result = " + result);
-        }
+        // int result = 0;
+        // for (int i = 0; i < 20; i++) {
+        //     result = result + 1;
+        //     System.out.println("result = " + result);
+        // }
 
-        int j = 2;
-        int result1 = 8;
-        while (j <= 5) {
-            result1 = result1 - j;
-            if (result1 >= -6) {
-                System.out.println("While - " + result1);
+        // int j = 2;
+        // int result1 = 8;
+        // while (j <= 5) {
+        //     result1 = result1 - j;
+        //     if (result1 >= -6) {
+        //         System.out.println("While - " + result1);
+        //     }
+        //     j++;
+        // }
+
+        int i = 10;
+        int sum = 0;
+        do {
+            sum = sum + i;
+            if(sum % 2 != 0) {
+                System.out.println("The number " + sum + " is Odd");
             }
-            j++;
-        }
+            i++;
+        } while(i <= 20);
 
-
+        System.out.println("sum of odd numbers - " + sum);
     }
 }
