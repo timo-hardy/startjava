4203d6ef7e74db874fbe208a2507adba0de1f7c6
Timur Khidirov <timos.gooogle@gmail.com>
2019-04-23 22:42:00 +0300

    Created a calculated

---
+++ Lesson_one/Calculator.java
@@ -0,0 +1,51 @@
+public class Calculator {
+    public static void main(String[] args) {
+        int firstNumber = 20;
+        int secondNumder = 10;
+
+        //Умножение
+        if(firstNumber > 0 && secondNumder > 0) {
+            System.out.println("Positive result = " + (firstNumber * secondNumder));
+        } else {
+            System.out.println("Negative result = " + (firstNumber * secondNumder));
+        }
+
+        //Вычетание
+        if (firstNumber > secondNumder && firstNumber > 0 && secondNumder > 0) {
+            System.out.println("Positive result = " + (firstNumber - secondNumder));
+        } else {
+            System.out.println("Negative result = " + (firstNumber - secondNumder));
+        }
+
+        //Сложение
+        if (firstNumber > secondNumder && firstNumber > secondNumder) {
+            System.out.println("Positive result = " + (firstNumber + secondNumder));
+        } else {
+            System.out.println("Negative result = " + (firstNumber + secondNumder));
+        }
+
+        //Деление
+        if (firstNumber >= 0 && secondNumder != 0) {
+            System.out.println("Positive or zero = " + (firstNumber / secondNumder));
+        } else if (secondNumder == 0) {
+            System.out.println("It is impossible to divide by zero");
+        }
+
+        //Деление по модулю
+        if (firstNumber >= 0 && secondNumder != 0) {
+            System.out.println("remainder of the division = " + (firstNumber % secondNumder));
+        } else if(secondNumder == 0) {
+            System.out.println("It is impossible to divide by zero");
+        }
+
+        //Возведение в степень
+        if (firstNumber >= 0) {
+            System.out.println("Positive result of exponentiation = " + (firstNumber * firstNumber));
+        } else {
+            System.out.println("Positive result of exponentiation = " + (firstNumber * firstNumber));
+        }
+
+
+
+    }
+}
