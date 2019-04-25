ab28630eb64a14aee48720713c10898ac80655be
Timur Khidirov <timos.gooogle@gmail.com>
2019-04-25 18:38:56 +0300

    Bug fix

--- Lesson_one/Calculator.java
+++ Lesson_one/Calculator.java
@@ -2,47 +2,43 @@ public class Calculator {
     public static void main(String[] args) {
         int firstNumber = 20;
         int secondNumder = 10;
+        int result;
 
         //Умножение
+        result = firstNumber * secondNumder;
         if(firstNumber > 0 && secondNumder > 0) {
-            System.out.println("Positive result = " + (firstNumber * secondNumder));
-        } else {
-            System.out.println("Negative result = " + (firstNumber * secondNumder));
+            System.out.println("Multiplication = " + result);
         }
 
         //Вычетание
-        if (firstNumber > secondNumder && firstNumber > 0 && secondNumder > 0) {
-            System.out.println("Positive result = " + (firstNumber - secondNumder));
-        } else {
-            System.out.println("Negative result = " + (firstNumber - secondNumder));
+        result = firstNumber - secondNumder;
+        if (firstNumber > 0 && secondNumder > 0) {
+            System.out.println("Subtraction = " + result);
         }
 
         //Сложение
-        if (firstNumber > secondNumder && firstNumber > secondNumder) {
-            System.out.println("Positive result = " + (firstNumber + secondNumder));
-        } else {
-            System.out.println("Negative result = " + (firstNumber + secondNumder));
+        result = firstNumber + secondNumder;
+        if (firstNumber > 0 && secondNumder > 0) {
+            System.out.println("Addition = " + result);
         }
 
         //Деление
+        result = firstNumber / secondNumder;
         if (firstNumber >= 0 && secondNumder != 0) {
-            System.out.println("Positive or zero = " + (firstNumber / secondNumder));
-        } else if (secondNumder == 0) {
-            System.out.println("It is impossible to divide by zero");
+            System.out.println("Division = " + result);
         }
 
         //Деление по модулю
+        result = firstNumber % secondNumder;
         if (firstNumber >= 0 && secondNumder != 0) {
-            System.out.println("remainder of the division = " + (firstNumber % secondNumder));
-        } else if(secondNumder == 0) {
-            System.out.println("It is impossible to divide by zero");
+            System.out.println("Remainder of the division = " + result);
         }
 
         //Возведение в степень
-        if (firstNumber >= 0) {
-            System.out.println("Positive result of exponentiation or zero = " + (firstNumber * firstNumber));
-        } else {
-            System.out.println("Negative result of exponentiation = " + (firstNumber * firstNumber));
+        result = 1;
+        for (int i = 1; i <= secondNumder; i++) {
+            result = result * firstNumber;
+            System.out.println(result);
         }
     }
 }
