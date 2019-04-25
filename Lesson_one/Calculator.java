ca583b105556f7ac648ade47650d6a731f56356d
Timur Khidirov <timos.gooogle@gmail.com>
2019-04-25 22:01:03 +0300

    Rewrite the code

--- Lesson_one/Calculator.java
+++ Lesson_one/Calculator.java
@@ -2,43 +2,25 @@ public class Calculator {
     public static void main(String[] args) {
         int firstNumber = 20;
         int secondNumder = 10;
+        char sign = '+';
         int result;
 
-        //Умножение
-        result = firstNumber * secondNumder;
-        if(firstNumber > 0 && secondNumder > 0) {
-            System.out.println("Multiplication = " + result);
-        }
-
-        //Вычетание
-        result = firstNumber - secondNumder;
-        if (firstNumber > 0 && secondNumder > 0) {
-            System.out.println("Subtraction = " + result);
-        }
-
-        //Сложение
-        result = firstNumber + secondNumder;
-        if (firstNumber > 0 && secondNumder > 0) {
-            System.out.println("Addition = " + result);
-        }
-
-        //Деление
-        result = firstNumber / secondNumder;
-        if (firstNumber >= 0 && secondNumder != 0) {
-            System.out.println("Division = " + result);
-        }
-
-        //Деление по модулю
-        result = firstNumber % secondNumder;
-        if (firstNumber >= 0 && secondNumder != 0) {
-            System.out.println("Remainder of the division = " + result);
-        }
-
-        //Возведение в степень
-        result = 1;
-        for (int i = 1; i <= secondNumder; i++) {
-            result = result * firstNumber;
-            System.out.println(result);
+        if (sign == '+') {
+            System.out.println(firstNumber + secondNumder);
+        } else if (sign == '-') {
+            System.out.println(firstNumber - secondNumder);
+        } else if (sign == '*') {
+            System.out.println(firstNumber * secondNumder);
+        } else if (sign == '/' && secondNumder != 0) {
+            System.out.println(firstNumber / secondNumder);
+        } else if (sign == '%' && secondNumder != 0) {
+            System.out.println(firstNumber % secondNumder);
+        } else if (sign == '^') {
+            result = 1;
+            for (int i = 1; i <= secondNumder; i++) {
+                result = result * firstNumber;
+                System.out.println(result);
+            }
         }
     }
 }
