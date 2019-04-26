e290b8bd9e66a908415fab72dea45ed597780f83
Timur Khidirov <timos.gooogle@gmail.com>
2019-04-26 15:41:43 +0300

    Created class MyFirstGame

---
+++ Lesson_one/MyFirstGame.java
@@ -0,0 +1,15 @@
+public class MyFirstGame {
+    public static void main(String[] args) {
+        int number = 1;
+        while (number <= 14) {
+            if (number < 14) {
+                System.out.println("Введенное вами число больше того, что загадал компьютер");
+            } else if (number > 14) {
+                System.out.println("Введенное вами число меньше того, что загадал компьютер");
+            } else {
+                System.out.println("Вы угадали!");
+            }
+            number++;
+        }
+    }
+}
