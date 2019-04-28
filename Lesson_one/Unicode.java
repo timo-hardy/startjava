3568df8f3e92fb1002211253d4d67058cf42942b
Timur Khidirov <timos.gooogle@gmail.com>
2019-04-28 22:03:15 +0300

    Created Unicode class

---
+++ Lesson_one/Unicode.java
@@ -0,0 +1,9 @@
+public class Unicode {
+    public static void main(String[] args) {
+        char symbols;
+        for (int i = 9398; i <= 10178; i++) {
+            symbols = (char) i;
+            System.out.println(symbols);
+        }
+    }
+}
