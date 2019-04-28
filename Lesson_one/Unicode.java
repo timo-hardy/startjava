5522e3167f8d93e8b52376449f4902bf9a73cefb
Timur Khidirov <timos.gooogle@gmail.com>
2019-04-28 22:06:29 +0300

    Bug fix

--- Lesson_one/Unicode.java
+++ Lesson_one/Unicode.java
@@ -1,6 +1,7 @@
 public class Unicode {
     public static void main(String[] args) {
         char symbols;
+
         for (int i = 9398; i <= 10178; i++) {
             symbols = (char) i;
             System.out.println(symbols);
