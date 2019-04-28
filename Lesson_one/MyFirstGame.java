975f483718cb17e7edf840273a76048f69dbb603
Timur Khidirov <timos.gooogle@gmail.com>
2019-04-28 20:22:09 +0300

    Buf fix

--- Lesson_one/MyFirstGame.java
+++ Lesson_one/MyFirstGame.java
@@ -1,17 +1,17 @@
 public class MyFirstGame {
     public static void main(String[] args) {
-        int computerNumber = 30;
-        int playerNumber = 40;
+        int computerNumber = 33;
+        int playerNumber = 16;
 
-        while (playerNumber >= computerNumber) {
+        while (playerNumber != computerNumber) {
             if (playerNumber < computerNumber) {
                 System.out.println("Введенное вами число меньше того, что загадал компьютер");
+                playerNumber++;
             } else if (playerNumber > computerNumber) {
                 System.out.println("Введенное вами число больше того, что загадал компьютер");
-            } else {
-                System.out.println("Вы угадали!");
+                playerNumber--;
             }
-            playerNumber--;
         }
+        System.out.println("Вы угадали!");
     }
 }
