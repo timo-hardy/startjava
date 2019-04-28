81ffe863f4ba1e2f83041270a7a7b219a69c84d0
Timur Khidirov <timos.gooogle@gmail.com>
2019-04-28 19:38:15 +0300

    Created two numbers and bug fix

--- Lesson_one/MyFirstGame.java
+++ Lesson_one/MyFirstGame.java
@@ -1,13 +1,17 @@
 public class MyFirstGame {
     public static void main(String[] args) {
-        int number = 1;
-        while (number <= 14) {
-            if (number < 14) {
+        int computerNumber = 30;
+        int playerNumber = 40;
+
+        while (playerNumber >= computerNumber) {
+            if (playerNumber < computerNumber) {
+                System.out.println("Введенное вами число меньше того, что загадал компьютер");
+            } else if (playerNumber > computerNumber) {
                 System.out.println("Введенное вами число больше того, что загадал компьютер");
             } else {
                 System.out.println("Вы угадали!");
             }
-            number++;
+            playerNumber--;
         }
     }
 }
