d9777e03f0295ea7ff1d2292426b1bb22174b9b1
Timur Khidirov <timos.gooogle@gmail.com>
2019-05-02 19:12:10 +0300

    Created Person class

---
+++ Lesson_two/Person.java
@@ -0,0 +1,23 @@
+public class Person {
+    String male = "Man";
+    String name = "Vasya";
+    int growth = 180;
+    int weight = 80;
+    int age = 34;
+
+    void go() {
+        System.out.println("Vasya goes");
+    }
+
+    void sit() {
+        System.out.println("Vasya sits");
+    }
+
+    String speek() {
+        return "Vasya speaks";
+    }
+
+    boolean run() {
+        return true;
+    }
+}
