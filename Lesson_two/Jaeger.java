ed9954f834fca44357c360fef7d41b6015ed1f18
timos-java <timos.gooogle@gmail.com>
2019-09-03 18:02:33 +0300

    Created two objects and bug fix

--- Lesson_two/Jaeger.java
+++ Lesson_two/Jaeger.java
@@ -3,11 +3,27 @@ public class Jaeger {
   private String name;
   private String mark;
   private String status;
-  private int height;
-  private int weight;
-  private int speed;
+  private double height;
+  private double weight;
 
   public void setName(String name) {
-    this.
+    this.name = name;
   }
+
+    public void setMark(String mark) {
+    this.mark = mark;
+  }
+
+    public void setStatus(String status) {
+    this.status = status;
+  }
+
+    public void setHeight(double height) {
+    this.height = height;
+  }
+
+    public void setWeight(double weight) {
+    this.weight = weight;
+  }
+
 }
--- Lesson_two/JaegerTest.java
+++ Lesson_two/JaegerTest.java
@@ -1,6 +1,18 @@
 public class JaegerTest {
 
   public static void main(String[] args) {
+    Jaeger gipsyDanger = new Jaeger();
+    gipsyDanger.setName("Gipsy Danger");
+    gipsyDanger.setMark("Mark-3");
+    gipsyDanger.setStatus("Destroyed");
+    gipsyDanger.setHeight(79.25);
+    gipsyDanger.setWeight(1.98);
 
+    Jaeger gipsyAvenger = new Jaeger();
+    gipsyAvenger.setName("Gipsy Avenger");
+    gipsyAvenger.setMark("Mark-6");
+    gipsyAvenger.setStatus("Destroyed");
+    gipsyAvenger.setHeight(81.77);
+    gipsyAvenger.setWeight(2.004);
   }
 }
