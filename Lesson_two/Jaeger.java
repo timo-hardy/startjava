dbb044cf6c141497cf08fd5f3e3b0ae8de5348a5
timos-java <timos.gooogle@gmail.com>
2019-09-03 17:32:19 +0300

    Implement classes Jaeger and JaegerTest

---
+++ Lesson_two/Jaeger.java
@@ -0,0 +1,13 @@
+public class Jaeger {
+
+  private String name;
+  private String mark;
+  private String status;
+  private int height;
+  private int weight;
+  private int speed;
+
+  public void setName(String name) {
+    this.
+  }
+}
---
+++ Lesson_two/JaegerTest.java
@@ -0,0 +1,6 @@
+public class JaegerTest {
+
+  public static void main(String[] args) {
+
+  }
+}
