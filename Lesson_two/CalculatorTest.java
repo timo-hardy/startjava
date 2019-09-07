6fd95df11f80bd490d065e3eaf5aec52075d75a3
timos-java <timos.gooogle@gmail.com>
2019-09-07 18:34:07 +0300

    Bug fix CalculatorTest

--- Lesson_two/CalculatorTest.java
+++ Lesson_two/CalculatorTest.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class CalculatorTest {
+
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		Calculator calculator = new Calculator();
