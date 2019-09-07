713ead9c23770e7fa9fcfb74d44638659472cc00
timos-java <timos.gooogle@gmail.com>
2019-09-07 19:45:00 +0300

    Bug fix

--- Lesson_two/GuessNumberTest.java
+++ Lesson_two/GuessNumberTest.java
@@ -1,9 +1,7 @@
 import java.util.Scanner;
 
 public class GuessNumber {
-  public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-
-
-  }
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+	}
 }
--- Lesson_two/Player.java
+++ Lesson_two/Player.java
@@ -1,3 +1,14 @@
 public class Player {
 
+	private String firstName;
+	private String secondName;
+	private int numberOne;
+	private int numberTwo;
+
+	public Player(String firstName, String secondName, int numberOne, int numberTwo) {
+		this.firstName = firstName;
+		this.secondName = secondName;
+		this.numberOne = numberOne;
+		this.numberTwo = numberTwo;
+	}
 }
