484b0ad53e5f24b969b364f2da41a88124a8bbad
timos-java <timos.gooogle@gmail.com>
2019-09-07 18:30:57 +0300

    Bug fix

--- Lesson_two/CalculatorTest.java
+++ Lesson_two/CalculatorTest.java
@@ -19,6 +19,7 @@ public class CalculatorTest {
 		System.out.print("Введите второе число: ");
 		int secondNumber = scanner.nextInt();
 		calculator.setSecondNumber(secondNumber);
+
         calculator.calculate();
 	}
 }
